e897dc5f49e569b145ae85c1a8b2aeb581879052
zuri2102 <zuriye45@gmail.com>
2024-07-17 13:54:36 -0700

    Initialization

---
+++ build/drone_sensing/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/mnt/c/Users/zuriy/Documents/bwsi-uav-good/flying_squirrels_architecture/install/drone_sensing'
