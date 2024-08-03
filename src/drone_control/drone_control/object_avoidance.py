d51227867d816e49036cc262c058b7645fed45bf
Strevino100 <144546840+Strevino100@users.noreply.github.com>
2024-08-02 21:59:22 -0400

    Update object_avoidance.py
    Updated version for 9 ROI's

--- src/drone_control/drone_control/object_avoidance.py
+++ src/drone_control/drone_control/object_avoidance.py
@@ -23,21 +23,29 @@ class ObjectAvoidance(Node):
         # message type, topic name, callback function, queue size
         self.real_position = self.create_subscription(VehicleOdometry, '/fmu/out/vehicle_odometry', self.position_callback, qos_profile)
 
-        self.center_sub = self.create_subscription(Float32, 'roi_center_distance', self.center_callback, 10)
-        self.left_sub = self.create_subscription(Float32, 'roi_left_distance', self.left_callback, 10)
-        self.right_sub = self.create_subscription(Float32, 'roi_right_distance', self.right_callback, 10)
-        self.top_sub = self.create_subscription(Float32, 'roi_top_distance', self.top_callback, 10)
-        self.bottom_sub = self.create_subscription(Float32, 'roi_bottom_distance', self.bottom_callback, 10)
-        
+        self.sub_1 = self.create_subscription(Float32, 'roi_1_distance', self.callback_1, 10)
+        self.sub_2 = self.create_subscription(Float32, 'roi_2_distance', self.callback_2, 10)
+        self.sub_3 = self.create_subscription(Float32, 'roi_3_distance', self.callback_3, 10)
+        self.sub_4 = self.create_subscription(Float32, 'roi_4_distance', self.callback_4, 10)
+        self.sub_5 = self.create_subscription(Float32, 'roi_5_distance', self.callback_5, 10)
+        self.sub_6 = self.create_subscription(Float32, 'roi_6_distance', self.callback_6, 10)
+        self.sub_7 = self.create_subscription(Float32, 'roi_7_distance', self.callback_7, 10)
+        self.sub_8 = self.create_subscription(Float32, 'roi_8_distance', self.callback_8, 10)
+        self.sub_9 = self.create_subscription(Float32, 'roi_9_distance', self.callback_9, 10)
+
         self.bool_publisher = self.create_publisher(Bool, 'close_or_not', 10)
         self.trajectoryPublisher = self.create_publisher(TrajectorySetpoint, '/fmu/in/trajectory_setpoint', 10)
         #publish if or if not too close
     
-        self.center_d = None
-        self.left_d = None
-        self.right_d = None
-        self.top_d = None
-        self.bottom_d = None
+        self.d1 = 0.0
+        self.d2 = 0.0
+        self.d3 = 0.0
+        self.d4 = 0.0
+        self.d5 = 0.0
+        self.d6 = 0.0
+        self.d7 = 0.0
+        self.d8 = 0.0
+        self.d9 = 0.0
         self.altitude_bool = True 
         self.xcord = 0.0   
         self.ycord = 0.0
@@ -47,7 +55,7 @@ class ObjectAvoidance(Node):
         self.get_logger().info('Object Avoidance Started')
 
 
-    def altitude_callback(self, msg):
+    def position_callback(self, msg):
         min_height = 0.3               #meters CHANGE to preferred value
         max_height = 2.9 - 0.8         #meters CHANGE to preferred value
 
@@ -58,25 +66,42 @@ class ObjectAvoidance(Node):
         if not (min_height <= self.zcord <= max_height):
             self.altitude_bool = False
             
-    def center_callback(self, msg):
-        self.center_d = msg.data
+    def callback_1(self, msg):
+        self.d1 = msg.data
         self.process_detections()
 
-    def left_callback(self, msg):
-        self.left_d = msg.data
+    def callback_2(self, msg):
+        self.d2 = msg.data
         self.process_detections()
 
-    def right_callback(self, msg):
-        self.right_d = msg.data
+    def callback_3(self, msg):
+        self.d3 = msg.data
         self.process_detections()
 
-    def top_callback(self, msg):
-        self.top_d = msg.data
+    def callback_4(self, msg):
+        self.d4 = msg.data
         self.process_detections()
 
-    def bottom_callback(self, msg):
-        self.bottom_d = msg.data
+    def callback_5(self, msg):
+        self.d5 = msg.data
         self.process_detections()
+
+    def callback_6(self, msg):
+        self.d6 = msg.data
+        self.process_detections()
+
+    def callback_7(self, msg):
+        self.d7 = msg.data
+        self.process_detections()
+
+    def callback_8(self, msg):
+        self.d8 = msg.data
+        self.process_detections()
+
+    def callback_9(self, msg):
+        self.d9 = msg.data
+        self.process_detections()
+
 
     def trajectorypublish(self):
         setpoint = TrajectorySetpoint()
@@ -89,32 +114,39 @@ class ObjectAvoidance(Node):
         increment = 0.05    #CHANGE to preferred value
         height = 2.9        #in meters
         min_distance = 1    #CHANGE to preferred value
+        up_counter = 0
+        down_counter = 0
+        middle_counter = 0
 
         if self.altitude_bool:
-            if self.top_d <= min_distance:
+            down_counter = sum(1 for d in {self.d1, self.d2, self.d3} if d <= min_distance)
+            up_counter = sum(1 for d in {self.d7, self.d8, self.d9} if d <= min_distance)
+            middle_counter = sum(1 for d in {self.d4, self.d5, self.d6} if d <= min_distance)
+
+            if down_counter > up_counter and down_counter > middle_counter:
                 print("Something ABOVE")
                 #move down
                 self.zcord -= increment
                 print("Moving DOWN")
 
-            elif self.bottom_d <= min_distance:
+            elif up_counter > down_counter and up_counter > middle_counter:
                 print("Something BELOW")
                 #move up
                 self.zcord += increment
                 print("Moving UP")
 
             else:
-                print("Horizontal blocked")
+                print("Referencing altitude...")
                 if self.zcord > 0.5 * height:
-                    print(f"{self.altitude}m, moving DOWN")
+                    print(f"{self.zcord}m, moving DOWN")
                     self.zcord -= increment
-
                 else:
-                    print(f"{self.altitude}m, moving UP")
+                    print(f"{self.zcord}m, moving UP")
                     self.zcord += increment
             self.target = [self.xcord, self.ycord, self.zcord]
             self.trajectorypublish()
-            self.bool_publisher.publish(self.altitude_bool)
+            bool_msg = Bool(data=self.altitude_bool)
+            self.bool_publisher.publish(bool_msg)
 
         
 def main(args=None):    
